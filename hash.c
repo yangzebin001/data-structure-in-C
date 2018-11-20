47ee79c3e1b4e1e868a550dfc0f34d2431a5621f
yangzebin <405678856@qq.com>
2018-11-20 23:15:59 +0800

    add hashtable file

---
+++ hash.c
@@ -0,0 +1,135 @@
+#include "hashsep.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MinTableSize 1
+
+struct ListNode{
+	ElementType Element;
+	Position Next;
+}; 
+
+typedef Position List;
+
+struct HashTbl{
+	int TableSize;
+	List *TheLists;
+};
+
+int Hash(int Key, int TableSize){
+	return Key % TableSize;
+}
+
+HashTable InitializeTable(int TableSize){
+	HashTable H;
+	int i;
+	if(TableSize < MinTableSize){
+		printf("tablesize error");
+		return NULL;
+	}
+	
+	H = malloc(sizeof(struct HashTbl));
+	if(H == NULL){
+		printf("init hashtable fail"); 
+		return NULL; 
+	} 
+	//H->TableSize = NextPrime(TableSize);
+	H->TableSize = TableSize;
+	
+	H->TheLists = malloc(sizeof(List) * H->TableSize);
+	
+	if(H->TheLists == NULL){
+		printf("init Thelists fail"); 
+		return NULL;
+	} 
+	 
+	for(i = 0; i < H->TableSize; i++){
+		H->TheLists[i] = malloc(sizeof(struct ListNode));
+		if(H->TheLists[i] == NULL){
+			printf("init Thelists fail"); 
+			return NULL;
+		}else{
+			H->TheLists[i]->Next = NULL;
+		}
+	} 
+	
+	return H;
+	 
+}
+
+
+Position Find(ElementType Key, HashTable H){
+	Position P;
+	List L;
+	
+	L = H->TheLists[Hash(Key,H->TableSize)];
+	P = L->Next;
+	while(P != NULL && P->Element != Key){
+		P = P->Next;
+	}
+	return P;
+}
+
+void Insert(ElementType Key, HashTable H){
+	Position Pos, NewCell;
+	List L;
+	Pos = Find(Key, H);
+	//not found key 
+	if(Pos == NULL){
+		NewCell = malloc(sizeof(struct ListNode));
+		if(NewCell == NULL){
+			printf("init NewCell fail"); 
+			return NULL;
+		} else{
+			L = H->TheLists[Hash(Key,H->TableSize)];
+			NewCell->Next = L->Next;
+			NewCell->Element = Key;
+			L->Next = NewCell;
+		}
+	}	
+}
+
+void TraverseNode(Position P){
+	while(P != NULL){
+		printf("%d\t",P->Element);
+		P = P->Next;
+	}
+	printf("\n");
+}
+
+void Traverse(HashTable H){
+	int i = 0;
+	for(;i < H->TableSize; i++){
+		if(H->TheLists[i]->Next == NULL){
+			printf("NULL\n");
+		}else{
+			TraverseNode(H->TheLists[i]->Next);
+		}
+	}
+}
+
+int main(){
+
+	HashTable h;
+	h = InitializeTable(5);
+	Insert(5,h);
+	Insert(15,h);
+	Insert(3,h);
+	Insert(2,h);
+	
+	Position p;
+	p = Find(3,h);
+	
+	Traverse(h);
+	TraverseNode(p);
+}
+
+
+
+
+
+
+
+
+
+
---
+++ hashsep.h
@@ -0,0 +1,19 @@
+#ifndef _HashSep_H
+
+typedef int ElementType;
+
+struct ListNode;
+typedef struct ListNode * Position;
+struct HashTbl;
+typedef struct HashTbl * HashTable;
+
+HashTable InitializeTable(int TableSize);
+void DestroyTable(HashTable H);
+Position Find(ElementType Key, HashTable H);
+void Insert(ElementType Key,HashTable H);
+void TraverseNode(Position P);
+void Traverse(HashTable H);
+
+int Hash(int Key, int TableSize);
+
+#endif 
